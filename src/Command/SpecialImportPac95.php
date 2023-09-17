5cb91a1c1701683bd4d81e85900ead8cd407fbe0
Josh Peters <github@joshp.me>
2023-09-17 22:09:12 +1000

    Special import for Pac95 to do authors and titles, and fix positions

--- src/Command/SpecialImportPac95.php
+++ src/Command/SpecialImportPac95.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use App\Entity\Author;
 use App\Entity\Conference;
 use App\Entity\Reference;
 use Doctrine\ORM\EntityManagerInterface;
@@ -27,32 +28,114 @@ class SpecialImportPac95 extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $data = file_get_contents("src/DataFixtures/Import/pac95.txt");
-        preg_match_all("/([0-9]+)\s+[.]{5,}\s+([A-Z]+[0-9]+)/u", $data, $matches);
-        $papers = [];
-        $totalPapers = count($matches[0]);
-        for($i = 0; $i < $totalPapers; $i++) {
-            $startPage = $matches[1][$i];
-            $paperCode = $matches[2][$i];
-            if ($i < $totalPapers - 1) {
-                $endPage = $matches[1][$i + 1] - 1;
-            } else {
-                $endPage = (int)($matches[1][$i]) + 3;
+
+        $lines = file("src/DataFixtures/Import/pac95.txt");
+
+        // if line doesnt end with a paper code remove the new line
+        foreach ($lines as $key => $line) {
+            if (!preg_match("/[A-Z]{3}[0-9]{2}/", $line)) {
+                $lines[$key] = rtrim($line) . " ";
             }
-            $papers[$paperCode] = $startPage . "-" . $endPage;
+        }
+        $content = implode("", $lines);
+
+        // check if there are two series of periods
+        $lines = explode("\n", $content);
+
+
+        $papers = [];
+        foreach ($lines as $line) {
+            assert(preg_match("/\.{3,}(.*)[A-Z0-9a-z](.*)\.{3,}/", $line) === 0);
+            $content = trim($line);
+
+            //Bunch Lengthening Thresholds on the Daresbury SRS — J.A. Clarke......................................... 3128 WAC23
+            if (preg_match("/(.*?)—(.*?)\.{2,}\s*([0-9]+)\s+([A-Z]{3}[0-9]{2})/", $content, $matches)) {
+                assert(filter_var($matches[3], FILTER_VALIDATE_INT) !== false);
+                $papers[$matches[4]] = [
+                    "title" => trim($matches[1]),
+                    "authors" => trim($matches[2]),
+                    "original_author_str" => trim($matches[2]),
+                    "page" => trim($matches[3]),
+                    "code" => trim($matches[4])
+                ];
+            } else {
+                $output->writeln("Did not match: $content");
+            }
         }
 
-        preg_match_all("/[.]{5,}\s+([0-9]+)\s+([A-Z]+[0-9]+)/u", $data, $matches);
-        $totalPapers = count($matches[0]);
-        for($i = 0; $i < $totalPapers; $i++) {
-            $startPage = $matches[1][$i];
-            $paperCode = $matches[2][$i];
-            if ($i < $totalPapers - 1) {
-                $endPage = $matches[1][$i + 1] - 1;
-            } else {
-                $endPage = (int)($matches[1][$i]) + 3;
+        // sort by page
+        uasort($papers, function ($a, $b) {
+            return $a['page'] <=> $b['page'];
+        });
+
+        // create page position
+        foreach ($papers as $code => &$paper) {
+            $nextPn = 0;
+            // find next higher page number
+            foreach ($papers as $pn) {
+                if (filter_var($pn['page'], FILTER_VALIDATE_INT) && $pn['page'] > $paper['page']) {
+                    $nextPn = $pn['page'] - 1;
+                    break;
+                }
             }
-            $papers[$paperCode] = $startPage . "-" . $endPage;
+            if ($nextPn == 0) {
+                $nextPn = $paper['page'] + 3;
+            }
+            $paper['position'] = $paper['page'] . "-" . $nextPn;
+        }
+
+        $fixAuthor = function($author) {
+            $author = trim($author);
+            $author = preg_replace("/^([A-Z])[a-z]+\s(([A-Z]\.\s)?[A-Z][A-Za-z']+)$/", "$1. $2", $author);
+            //assert($fixAuthor("Yong-Chul Chae") === "Y. C. Chae");
+            $author = preg_replace("/^([A-Z])[a-z]+\-([A-Z])[a-z]+\s([A-Z][A-Za-z']+)$/", "$1. $2. $3", $author);
+            // assert($fixAuthor("J.A. Clarke") === "J. A. Clarke");
+            $author = preg_replace("/^([A-Z][a-z]*)\.([A-Z])\.\s([A-Z][A-Za-z']+)$/", "$1. $2. $3", $author);
+            return trim($author);
+        };
+
+
+        assert($fixAuthor("J.A. Clarke") === "J. A. Clarke");
+        assert($fixAuthor("Glenn Decker") === "G. Decker");
+        assert($fixAuthor("Yong-Chul Chae") === "Y. C. Chae");
+        assert($fixAuthor("Steven J. Werkema") === "S. J. Werkema");
+        assert($fixAuthor("Ian Hsu") === "I. Hsu");
+        assert($fixAuthor("Yu.P. Virchenko") === "Yu. P. Virchenko");
+        assert($fixAuthor("D.-P. Deng") === "D.-P. Deng");
+        assert($fixAuthor("Patrick G. O'Shea") === "P. G. O'Shea");
+
+        $authorStr = function($authors) {
+            $firstAuthor = $authors[0];
+            // Reform author text to be only the required content.
+            if (count($authors) > 6) {
+                $text = $firstAuthor . " et al.";
+            } else {
+                if (count($authors) <= 2) {
+                    $text = implode(" and ", $authors);
+                } else {
+                    // oxford comma
+                    $text = implode(', ', array_slice($authors, 0, -1)) . ', and ' . end($authors);
+                }
+            }
+            return $text;
+        };
+
+        // find authors
+        foreach ($papers as &$paper) {
+            $authors = explode(",", $paper['authors']);
+            foreach ($authors as &$author) {
+                $author = $fixAuthor($author);
+            }
+            $paper['authors'] = $authors;
+
+            $paper['author_str'] = $authorStr($authors);
+        }
+
+
+        // fix paper titles
+        foreach ($papers as &$paper) {
+            $paper['title'] = str_replace(" (Invited)", "",$paper['title']);
+            $paper['title'] = preg_replace("/\s+/", " ", $paper['title']);
         }
 
         /**
@@ -68,9 +151,53 @@ class SpecialImportPac95 extends Command
         foreach ($references as $reference) {
             $code = $reference->getPaperId();
             if (isset($papers[$code])) {
-                $reference->setPosition($papers[$code]);
+
+                if ($reference->getTitle() !== $papers[$code]['title']) {
+
+                    $output->writeln("Title mismatch: " . $reference->getTitle() . " vs " . $papers[$code]['title']);
+                    $reference->setTitle($papers[$code]['title']);
+                }
+
+                if ($reference->getPosition() !== $papers[$code]['position']) {
+                    $output->writeln("Position mismatch: " . $reference->getPosition() . " vs " . $papers[$code]['position']);
+                    $reference->setPosition($papers[$code]['position']);
+                }
+
+                if ($reference->getAuthor() !== $papers[$code]['author_str']) {
+                    $output->writeln("Author mismatch: " . $reference->getAuthor() . " vs " . $papers[$code]['author_str']);
+                    $reference->setAuthor($papers[$code]['author_str']);
+                }
+
+                $reference->setOriginalAuthors($papers[$code]['original_author_str']);
+
+                foreach ($reference->getAuthors() as $author) {
+                    $author->removeReference($reference);
+                    if ($author->getReferences()->count() == 0) {
+                        $this->manager->remove($author);
+                        $output->writeln("Removed author: " . $author->getName());
+                    }
+                }
+                foreach ($papers[$code]['authors'] as $result) {
+                    $foundAuthors = $this->manager->getRepository(Author::class)->findBy(["name" => $result]);
+                    if (count($foundAuthors) > 0) {
+                        $foundAuthor = $foundAuthors[0];
+                    } else {
+                        $foundAuthor = new Author();
+                        $foundAuthor->setName($result);
+                        $this->manager->persist($foundAuthor);
+                        $output->writeln("New author: " . $result);
+                    }
+
+                    if (!$foundAuthor->getReferences()->contains($reference)) {
+                        $foundAuthor->addReference($reference);
+                    }
+                    $reference->addAuthor($foundAuthor);
+                }
+
                 $reference->setCache($reference->__toString());
                 $changes++;
+            } else {
+                $output->writeln("Missing: " . $code);
             }
 
             if ($changes % 100 == 0) {
